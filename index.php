3be77388eed0a5c21d0c6805b8b29b962374eb45
himdan <himdanchhimi@gmail.com>
2016-08-14 13:44:59 +0100

    TG-441: add some content in the application index

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php 
-echo phpinfo();
+/*
+@Author himdanchhimi@gamil.com
+@Version 1.0.0
+@Name hello world application
+*/
 ?>
+<Doctype html>
+<html charset="utf-8" lang="fr-fr">
+<head>
+</head>
+<body>
+<!--Bootstrap design  -->
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<h1>Hello Everyone!</h1>
+</div>
+<div class="col-lg-12">
+<p class="text text-intro">this is a intiation project for Git all i presume to do is to enhance my level of expertise in Git </p>
+</div>
+<div class="col-lg-12">
+<p cass="text text-salut"> thanks for your attention</p>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
